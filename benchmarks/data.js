c331deca972d1777a50a64eed9b941301f639347
github-action-benchmark <github@users.noreply.github.com>
2024-12-11 20:00:48 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 8060691804184c3a6e1406f7b28a13735aed5a80

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733066570001,
+  "lastUpdate": 1733947248533,
   "repoUrl": "https://github.com/massongit/textlint",
   "entries": {
     "Benchmark": [
@@ -43932,6 +43932,48 @@ window.BENCHMARK_DATA = {
             "value": 0.7279710520400001,
             "unit": "seconds",
             "range": "± 0.016360038999999937"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "29139614+renovate[bot]@users.noreply.github.com",
+            "name": "renovate[bot]",
+            "username": "renovate[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8060691804184c3a6e1406f7b28a13735aed5a80",
+          "message": "fix(deps): update dependency table to ^6.9.0 (#1444)\n\nCo-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
+          "timestamp": "2024-12-10T22:25:14Z",
+          "tree_id": "0c59ef8ef68ac434d319d50a7d442a58eb9b1de0",
+          "url": "https://github.com/massongit/textlint/commit/8060691804184c3a6e1406f7b28a13735aed5a80"
+        },
+        "date": 1733947245872,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "npm run bench:self",
+            "value": 3.0657071131599993,
+            "unit": "seconds",
+            "range": "± 0.04865564599999983"
+          },
+          {
+            "name": "npm run bench:technical-writing",
+            "value": 6.522976883160001,
+            "unit": "seconds",
+            "range": "± 0.1599540029999993"
+          },
+          {
+            "name": "npm run bench:jtf-style",
+            "value": 0.71151726956,
+            "unit": "seconds",
+            "range": "± 0.013866542000000037"
           }
         ]
       }
